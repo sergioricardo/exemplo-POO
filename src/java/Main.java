ce7a720cb2178011b5bc562158ffa69ca0ec63e4
sergio <sergiofatec@gmail.com>
2023-04-29 20:53:41 -0300

    teste de conexão

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,11 +1,17 @@
 package java;
 
 public class Main {
-/*	//Descrição do Desafio
+	public static void main(String[] args) {
+		
+	}
+	
+	/*	//Descrição do Desafio
 	//Desmistifique a Programação Orientada a Objetos (POO) com Java e pratique esse conceito fundamental nesse desafio. Para isso, os pilares da OO são devidamente formalizados: Abstração, Encapsulamento, Herança e Polimorfismo. Neste desafio, você poderá desenvolver sua capacidade de abstração com um problema real e implementar as evoluções que achar interessantes wink
 
 	Dica: você pode dar um "fork" no Repositório do GitHub para organizar suas alterações e evoluções, mantendo uma referência direta ao código original.
 
 	Links Úteis
 	Repositório no GitHub (README e implementação de referência)*/
+	
+
 }
